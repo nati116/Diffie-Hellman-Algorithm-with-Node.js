0046f0ed60b22b91bc35e648330e4053a57d6da6
Nathnael Bassa <bassanati1@gmail.com>
2023-08-05 10:55:58 +0300

    Final

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
-const crypto = require('crypto');
-    const p = 23;
-    const g = 9;
 
-    // alice and bob private keys
-    const a = crypto.randomBytes(32).readUIntBE(0,32);
-    const b = crypto.randomBytes(32).readUIntBE(0,32);
+const crypto = require('crypto'); 
 
-    // alice public key
-    var x = g ** a % p;
+class DHGenParameterSpec {
+  static p;
+  static g;
+  static a;
+  static b;
+  static A;
+  static B;
 
-    // bob public key generated
+  static getPrimeP_G() {
+    const rand1 = crypto.randomBytes(32);
+    const rand2 = crypto.randomBytes(32);
 
-    var y = g ** b % p;
-  
-   // shared secret key
+    this.p = BigInt('0x' + rand1.toString('hex'));
+    this.g = BigInt('0x' + rand2.toString('hex'));
 
-    const ka = y**a % p;
-    const kb = x**b % p;
+    console.log(`${this.p},${this.g}`);
+  }
 
+  static getExponent() {
+    this.a = crypto.randomInt(1000);
+    this.b = crypto.randomInt(1000);
 
+    console.log(`${this.a}__${this.b}`);
+  }
 
-   if (ka === kb ){
-        console.log('your shared secrete keys match perfectly');
-   }
+  static pow() {
+    this.A = BigInt(this.g) ** BigInt(this.a) % this.p; 
+    this.B = BigInt(this.g) ** BigInt(this.b) % this.p;
+  }
+}
 
+Execute = () => {
+  DHGenParameterSpec.getPrimeP_G();
+  DHGenParameterSpec.getExponent();
+  DHGenParameterSpec.pow();
+
+  const Sa = BigInt(DHGenParameterSpec.B) ** BigInt(DHGenParameterSpec.a) % DHGenParameterSpec.p;
+  const Sb = BigInt(DHGenParameterSpec.A) ** BigInt(DHGenParameterSpec.b) % DHGenParameterSpec.p;
+
+  console.log(`${DHGenParameterSpec.A}__${DHGenParameterSpec.B}`);
+  if( Sa === Sb){
+     console.log('the shared keys are the same');
+  }
+  console.log(`${Sa}__${Sb}`);
+}
+
+Execute();
 
 
